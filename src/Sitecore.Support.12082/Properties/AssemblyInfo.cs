1dcce5896b12444ff97894d04d9436387c6706fc
DK\IRO <iro@sitecore.net>
2018-08-08 16:52:35 +0300

    Initial

---
+++ src/Sitecore.Support.12082/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.12082")]
+[assembly: AssemblyProduct("Sitecore.Support.12082")]
+[assembly: ComVisible(false)]
